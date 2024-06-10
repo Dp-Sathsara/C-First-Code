2cf610cf9d2b748f1c52f74cd6bf15732617e73d
Dp-Sathsara <desithas2021@gmail.com>
2024-06-10 21:42:38 +0530

    Em class

---
+++ new.cs
@@ -0,0 +1,14 @@
+using System;
+
+struct Employee{
+   public int Id { get; set; }
+   public string FullName { get; set; }
+}
+
+public class MyClass{
+ public static void Main(){
+   Employee obj= new Employee();
+   obj.FullName = "Hello World";
+   Console.WriteLine(obj.FullName);
+  }
+}
